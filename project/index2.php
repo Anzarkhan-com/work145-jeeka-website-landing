06367237cfa7d890d6f4f9b2da1ec2dd2dd34bc8
fardin <frdinshaikh949@gmail.com>
2023-05-18 15:39:17 +0530

    new design

---
+++ project/index2.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jeeka landing page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+
+<body>
+    <section class="head-jeeka blue">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="width-20">
+                        <div class="logo">
+                            <img src="images/jeeka logo.jpg" alt="jeeka logo" style="height:2.46cm; width:2.14cm ">
+                        </div>
+                        <div class="head-para">
+                            <a href="index2.php">PHTHALOCYANINES</a>
+                            <h6> <i>ε Epsilon</i></h6>
+                            <h6><i>β Beta</i></h6>
+                            <h6><i>α Alpha</i></h6>
+                        </div>
+                        <div class="contact">
+                            <h3 class="bold">CONTACT US</h3>
+                            <h6 class="bold">Mr. Jeeken Kharawala</h6>
+                            <p><i class="fa-solid fa-location-dot me-3"></i>380 Applewoods Villas | SP Ring Road | Ahmedabad -380 058. INDIA</p>
+                            <p><i class="fa-solid fa-phone me-3"></i>+91 9879619739</p>
+                            <p><i class="fa-regular fa-envelope me-3"></i>jeeken@jeeka.com</p>
+                        </div>
+                        <div class="about-us">
+                            <h3 class="bold">ABOUT US</h3>
+                            <p class="yellow">"We represent the <span class="bg-danger"> 4th generation </span> of the B.T.KHARAWALA group, a company originally established by Bhagwanlal Tejaji Kharawala in 1917."</p>
+
+                        </div>
+                        <div class="para">
+                            <p>JEEKA INDUSTRIES was established in 1957. From an early foray into manufacturing of Direct & Basic Dyes, the JEEKA group today has a strong presence in the Manufacturing of Phthalocyanine Blue Pigments.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-sm-6">
+                    <div class="first-page">
+                        <div class="para">
+                            <p><span class="bold">JEEKA</span> is a recognized leader in manufacturing of Phthalocyanine Pigments for Coatings, Inks, Plastics/Fibres & Color Filters. </p>
+                            <p>Our success is driven by our long-term strategy of continuous operational improvements. The Quality of <span class="bold">J</span>EEKA<i><span class="bold">B</span>LUE </i>is the result of exceptional diligence in the production process. </p>
+                        </div>
+                        <div class="manufactured">
+                            <h5><a href="#">Manufactured Pigments:</a></h5>
+                            <div class="epsilon">
+                                <h5 class="bold">Pigment Blue 15:6 | ε Epsilon </h5>
+                                <p><i>PB 15:6 has a Reddish hue, a high clearness and a high Tinting strength compared to the Alpha crystal form and its Solvent resistance against the crystal growth of primary crystals is higher than that of Beta crystal form. Therefore the Epsilon crystal form has remarkably excellent properties with less apprehension about a change of Hue and decrease in Tinting strength and clearness.</i></p>
+                                <p><i>PB 15:6 is recommended for Solvent & Water based Automotive, Industrial & Decorative Paints, Digital Inks and Color Filters in LCD panels. </i></p>
+                            </div>
+                            <div class="beta-15-3">
+                                <h5 class="bold">Pigment Blue 15:3 | β Beta</h5>
+                                <p><i>PB 15:3 is a greenish blue pigment with excellent Heat stability and Fastness properties. It is suitable for Inks [Solvent & Water based Gravure & Flexo], Paints [Aqueous, Solvent & Air Drying systems] and Plastics [Polyolefins, PP -Fibres ] </i> </p>
+                            </div>
+                            <div class="beta-15-4">
+                                <h5 class="bold">Pigment Blue 15:4 | β Beta</h5>
+                                <p><i>PB 15:4 is a non-flocculating bright greenish blue pigment suitable for Liquid Inks [Gravure & Flexo] and Coatings [Industrial & Automotive]</i></p>
+                            </div>
+                        </div>
+                        <div class="trading">
+                            <h5><a href="#">Trading & Distribution:</a></h5>
+                            <h5 class="bold">Pigments</h5>
+                            <h5>Pigment Blue 15:0 | α Alpha</h5>
+                            <h5>Pigment Blue 15:1 | α Alpha</h5>
+                            <h5>Pigment Blue 15:2 | α Alpha</h5>
+                        </div>
+                    </div>
+                    <div class="second-page text-center my-5 py-5">
+                        <h4 class="bold">|OUR DISTRIBUTORS|</h4>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-sm-12">
+                                    <div class="germany my-5">
+                                        <h5><a href="#" class="bold blue">GERMANY</a></h5>
+                                        <h5 class="bold">PENPET PETROCHEMICAL TRADING GmbH</h5>
+                                        <p><i class="fa-solid fa-location-dot me-3"></i>Merkurring 105 D-22143 Hamburg.</p>
+                                        <p><i class="fa-solid fa-phone me-3"></i>+49 40 67579930</p>
+                                        <p><i class="fa-regular fa-envelope me-3"></i>cm@penpet.dewww.penpet.de</p>
+                                    </div>
+                                    <div class="germany my-5">
+                                        <h5><a href="#" class="bold blue">ISRAEL</a></h5>
+                                        <h5 class="bold">M.I.Chemicals Ltd.</h5>
+                                        <p><i class="fa-solid fa-location-dot me-3"></i>Meiron Str.96 Kvar Vradim 25147 </p>
+                                        <p><i class="fa-solid fa-phone me-3"></i>+972 525208887</p>
+                                        <p><i class="fa-regular fa-envelope me-3"></i>ts@mi-chemicals.com</p>
+                                    </div>
+                                    <div class="germany my-5">
+                                        <h5><a href="#" class="bold blue">ITALY</a></h5>
+                                        <h5 class="bold">AZELIS Italia S.r.l.</h5>
+                                        <p><i class="fa-solid fa-location-dot me-3"></i>Via Tommaso Gulli, 3920147 Milano.</p>
+                                        <p><i class="fa-solid fa-phone me-3"></i>+39 348 2720722</p>
+                                        <p><i class="fa-regular fa-envelope me-3"></i>marco.gerosa@azelis.itwww.azelis.com</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="germany my-5">
+                                        <h5><a href="#" class="bold blue">KOREA</a></h5>
+                                        <h5 class="bold">Penpet Korea Co.Ltd</h5>
+                                        <p><i class="fa-solid fa-location-dot me-3"></i>D-701 26, Beobwon-ro 9-gil,Songpa-guSeoul 05836.</p>
+                                        <p><i class="fa-solid fa-phone me-3"></i>+82 02.6425.0510</p>
+                                        <p><i class="fa-regular fa-envelope me-3"></i>harry@penpet.co.krwww.penpet.de</p>
+                                    </div>
+                                    <div class="germany my-5">
+                                        <h5><a href="#" class="bold blue">THAILAND</a></h5>
+                                        <h5 class="bold">YAMAMOTO TRADING[Thailand] Co.Ltd</h5>
+                                        <p><i class="fa-solid fa-location-dot me-3"></i>99/6 Moo13, Krisdanakorn 21 Km. 7,Bangna-Trad Road, Bangkaew, BangpleeSamutprakarn 10540.</p>
+                                        <p><i class="fa-solid fa-phone me-3"></i>+66 02-740-9336 to 9</p>
+                                        <p><i class="fa-regular fa-envelope me-3"></i>wiboondej@ytc.thailand.co.thwww.ytc-j.co.jp</p>
+                                    </div>
+                                    <div class="germany my-5">
+                                        <h5><a href="#" class="bold blue">USA</a></h5>
+                                        <h5 class="bold">GR Pigment</h5>
+                                        <p><i class="fa-solid fa-location-dot me-3"></i>2711 Cedarglade Dr.Naperville IL 60564</p>
+                                        <p><i class="fa-solid fa-phone me-3"></i>+1 630 303 0067</p>
+                                        <p><i class="fa-regular fa-envelope me-3"></i>grpigment@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="germany my-5">
+                                <h5><a href="#" class="bold blue">VIETNAM</a></h5>
+                                <h5 class="bold">YAMAMOTO TRADING [Thailand] Co.Ltd.</h5>
+                                <p><i class="fa-solid fa-location-dot me-3"></i>Ground Floor, 70 Pham Ngoc Thach Street District 3, Ho Chi Minh City</p>
+                                <p><i class="fa-solid fa-phone me-3"></i>+66 02-740-9336 to 9</p>
+                                <p><i class="fa-regular fa-envelope me-3"></i>wiboondej@ytc.thailand.co.thwww.ytc-j.co.jp</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- <section class="recognized text-primary">
+        <div class="container">
+            <p><b>JEEKA</b> is a recognized leader in manufacturing of Phthalocyanine Pigments for Coatings, Inks, Plastics/Fibres & Color Filters. </p>
+            
+        </div>
+    </section> -->
+</body>
+
+</html>
